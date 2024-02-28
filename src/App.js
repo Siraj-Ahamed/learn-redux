f84d7b0d9f42bf6cf28129f01fa3642d1d3d8196
Siraj-Ahamed <ahamedsiraj770@gmail.com>
2024-02-28 22:56:13 +0530

    implement basic counter app

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
-import { compose, pipe } from "lodash/fp";
-import "./App.css";
+import Counter from "./features/counter/Counter";
 
-const person = {
-    name: "Siraj",
-    address: {
-        city: "Mannar",
-        country: "Sri Lanka",
-    },
-};
-
-const updated = { ...person, name: "Ahamed",
-address : {
-  ...person.address, country: 'Malaysia'
-} };
-
-// updated.address.country = "Dubai";
-
-console.log("person", person);
-console.log("updated", updated);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let input = "   Subcribe  ";
-
-// const trim = (str) => str.trim();
-// const toLowerCase = (str) => str.toLowerCase();
-// const wrapCustomize = (type) => (str) => `<${type}>${str}<${type}/>`;
-
-// const transform = pipe(trim, toLowerCase, wrapCustomize("span"));
-// console.log(transform(input));
-
-function App() {}
+function App() {
+  return (
+    <main>
+      <Counter />
+    </main>
+  )
+}
 
 export default App;
